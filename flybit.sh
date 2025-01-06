96acaad4548141d79a2dacc3962408aae6d3edc1
FlyBitVIP <kabsrsynabsrst@gmail.com>
2025-01-06 20:18:26 +0800

    Update flybit.sh

--- flybit.sh
+++ flybit.sh
@@ -68,8 +68,8 @@ install() {
 	echo $LINE
 	
 	# 下载文件
-	wget –no-cache -O flybit-agent.tar.gz $AGENT_URL
-	wget –no-cache -O /etc/systemd/system/flybit.service $SERVICE_URL
+	curl -L --no-cache -o flybit-agent.tar.gz $AGENT_URL
+	curl -L --no-cache -o /etc/systemd/system/flybit.service $SERVICE_URL
 	echo $LINE
 	success "下载成功"
 	echo $LINE
@@ -193,7 +193,7 @@ open_shell() {
 
 # 更新脚本
 upgrade_shell() {
-	wget –no-cache -O /usr/bin/flybit $SHELL_URL
+	curl -L --no-cache -o /usr/bin/flybit $SHELL_URL
 	success "更新成功"
 	chmod +x /usr/bin/flybit
 	open_shell
@@ -222,7 +222,7 @@ uninstall_shell() {
 	rm -rf /usr/bin/flybit
 	echo ""
 	info "卸载脚本成功！"
-	info "重新安装脚本命令: wget –no-cache -O /usr/bin/flybit $SHELL_URL&&chmod +x /usr/bin/flybit&&flybit"
+	info "重新安装脚本命令: curl -L --no-cache -o /usr/bin/flybit $SHELL_URL && chmod +x /usr/bin/flybit && flybit"
 }
 
 install_shell
