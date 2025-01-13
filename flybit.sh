8d1bb90f3239e45cf7a114f46b064cfaf169263d
FlyBitVIP <kabsrsynabsrst@gmail.com>
2025-01-13 23:00:01 +0800

    Update flybit.sh

--- flybit.sh
+++ flybit.sh
@@ -17,6 +17,53 @@ yellow='\033[0;33m'
 plain='\033[0m'
 LINE="=================================="
 
+check() {
+  # 检查当前用户是否为root
+  [[ $EUID -ne 0 ]] && echo -e "${red}错误：${plain} 必须使用root用户运行此脚本！\n" && exit 1
+  # check os
+  if [[ -f /etc/redhat-release ]]; then
+      release="centos"
+  elif cat /etc/issue | grep -Eqi "alpine"; then
+      release="alpine"
+      echo -e "${red}脚本暂不支持alpine系统！${plain}\n" && exit 1
+  elif cat /etc/issue | grep -Eqi "debian"; then
+      release="debian"
+  elif cat /etc/issue | grep -Eqi "ubuntu"; then
+      release="ubuntu"
+  elif cat /etc/issue | grep -Eqi "centos|red hat|redhat|rocky|alma|oracle linux"; then
+      release="centos"
+  elif cat /proc/version | grep -Eqi "debian"; then
+      release="debian"
+  elif cat /proc/version | grep -Eqi "ubuntu"; then
+      release="ubuntu"
+  elif cat /proc/version | grep -Eqi "centos|red hat|redhat|rocky|alma|oracle linux"; then
+      release="centos"
+  elif cat /proc/version | grep -Eqi "arch"; then
+      release="arch"
+  else
+      echo -e "${red}未检测到系统版本，请联系脚本作者！${plain}\n" && exit 1
+  fi
+
+  arch=$(uname -m)
+
+  if [[ $arch == "x86_64" || $arch == "x64" || $arch == "amd64" ]]; then
+      arch="64"
+  elif [[ $arch == "aarch64" || $arch == "arm64" ]]; then
+      arch="arm64-v8a"
+  elif [[ $arch == "s390x" ]]; then
+      arch="s390x"
+  else
+      arch="64"
+      echo -e "${red}检测架构失败，使用默认架构: ${arch}${plain}"
+  fi
+
+  # 检查是否为64位系统
+  if [ "$(getconf WORD_BIT)" != '32' ] && [ "$(getconf LONG_BIT)" != '64' ] ; then
+      echo "本软件不支持 32 位系统(x86)，请使用 64 位系统(x86_64)，如果检测有误，请联系作者"
+      exit 2
+  fi
+}
+
 # 输出红色
 err() {
     printf "${red}%s${plain}\n" "$*" >&2
@@ -41,7 +88,7 @@ check_flybit_shell() {
 check_env() {
 	echo $LINE
 	echo "开始检查依赖"
-    deps="wget grep systemctl curl"
+    deps="wget grep systemctl"
     for dep in $deps; do
         if ! command -v "$dep" >/dev/null 2>&1; then
             err "不支持$dep, 请先安装$dep"
@@ -90,11 +137,14 @@ install() {
 	
 	# 启动服务
 	systemctl start flybit
-	
-	echo $LINE
-	success "已成功安装并启动服务"
-	echo $LINE
-	open_shell
+
+	if [ $# -eq 0 ]; then
+	  success "安装成功"
+	  exit 0;
+	else
+    TEMP_MSG="已成功安装并启动服务"
+    display_menu
+	fi
 }
 
 # 卸载
@@ -112,10 +162,8 @@ uninstall() {
 	# 删除文件夹
 	rm -rf $FLYBIT_HOME
 	info "删除文件夹成功"
-	# 卸载脚本
-	uninstall_shell
 	echo $LINE
-	success "已彻底卸载服务（一个文件不留）"
+	success "卸载成功"
 }
 
 # 获取服务状态
@@ -155,12 +203,12 @@ display_menu() {
 		server_version=$("$FLYBIT_HOME/flybit-agent" version 2>&1)
 		success "当前服务版本: V$server_version"
 		if [[ "2" = "${status_code}" ]]; then
-			err "服务未启动"
+			err "当前状态：服务未启动"
 		else
-			success "服务运行中"
+			success "当前状态：服务运行中"
 		fi
 	else
-		err "未安装服务"
+		err "当前状态：未安装服务"
 	fi
 	
 	# 命令信息
@@ -168,23 +216,41 @@ display_menu() {
 	info "请选择操作："
 	echo $LINE
 	
-	success "1. 开始服务"
-	success "2. 停止服务"
+	if [[ "3" == "${status_code}" ]]; then
+		success "1. 停止服务"
+		success "2. 重启服务"
+	fi
+	if [[ "2" == "${status_code}" ]]; then
+		success "1. 开始服务"
+	fi
 	success "3. 更新状态"
-	success "4. 查看日志"
+	if [[ "1" != "${status_code}" ]]; then
+		success "4. 查看日志"
+		success "5. 清空日志"
+		success "6. 配置管理"
+	fi
 	success ""
 	if check_flybit_shell;then
 		success "7. 更新脚本"
 		err "8. 卸载脚本"
 	fi
+	
+	
 	if [[ "1" = "${status_code}" ]]; then
 		success "9. 安装服务"
 	else
 		err "9. 卸载服务"
 	fi
 	success ""
-	err "0. 退出"
+	success "10. BBR"
+	success ""
+	info "0. 退出"
 	echo $LINE
+	if [ $TEMP_MSG ]; then
+		echo $TEMP_MSG
+		unset TEMP_MSG
+		echo $LINE
+	fi
 	deal_userinput
 }
 
@@ -195,22 +261,52 @@ deal_userinput () {
 	echo $LINE
 	# 可以根据用户的选择执行不同的操作
 	case "$choice" in
-		1) systemctl start flybit && display_menu ;;
-		2) systemctl stop flybit && display_menu ;;
-		3) display_menu ;;
+		1) 
+			if [[ "3" == "${status_code}" ]]; then
+				systemctl stop flybit
+				TEMP_MSG="已停止服务"
+			fi
+			if [[ "2" == "${status_code}" ]]; then
+				systemctl start flybit
+				TEMP_MSG="已启动服务"
+			fi
+			display_menu
+			;;
+		2) 
+			systemctl restart flybit
+			TEMP_MSG="已重启服务"
+			display_menu 
+			;;
+		3) 
+			TEMP_MSG="更新状态成功"
+			display_menu 
+			;;
 		4) 
 			clear
 			tail -fn 30 $FLYBIT_HOME/logs/info.log
 			;;
-		7) upgrade_shell ;;
+		5) 
+			rm -rf $FLYBIT_HOME/logs/*
+			TEMP_MSG="清空日志成功"
+			display_menu
+			;;
+	  6) display_config;;
+		7) upgrade_shell 1;;
 		8) uninstall_shell ;;
 		9)  
 			if [[ "1" == "${status_code}" ]]; then
-				install
+				install 1
 			else
 				uninstall
 			fi
 			;;
+	  10)
+	    wget -N --no-check-certificate "https://raw.githubusercontent.com/chiakge/Linux-NetSpeed/master/tcp.sh"
+	    chmod +x tcp.sh
+	    ./tcp.sh
+	    rm -f ./tcp.sh
+	    display_menu
+	    ;;
 		0) 
 			info "退出"
 			exit 0
@@ -219,9 +315,58 @@ deal_userinput () {
 	esac
 }
 
+# 显示配置菜单
+display_config() {
+  clear
+  echo $LINE
+  info "配置信息"
+  echo $LINE
+  cd $FLYBIT_HOME
+  ./flybit-agent config print
+  echo $LINE
+  info "请选择操作："
+  echo ""
+  success "1. 新增配置(服务地址一样进行替换)"
+  err "2. 删除配置"
+  echo ""
+  info "0. 返回上一级"
+  echo $LINE
+  if [ $TEMP_MSG ]; then
+  		echo $TEMP_MSG
+  		unset TEMP_MSG
+  		echo $LINE
+  	fi
+  # 等待用户输入
+  read -p "请输入你的选择: "  choice
+  echo $LINE
+  # 可以根据用户的选择执行不同的操作
+  case "$choice" in
+    1) newConfig;;
+    2) deleteConfig;;
+    0) display_menu;;
+  esac
+}
+
+# 删除配置
+deleteConfig() {
+  read -p "请输入需要删除配置的编号: "  choice
+  cd $FLYBIT_HOME
+  TEMP_MSG=$(./flybit-agent config remove "$choice" 2>&1)
+  display_config
+}
+
+# 新增配置
+newConfig() {
+  read -p "请输入对接地址: "  NEW_CONFIG_HOST
+  read -p "请输入ID: "  NEW_CONFIG_ID
+  read -p "请输入密钥: "  NEW_CONFIG_KEY
+  TEMP_MSG=$(./flybit-agent config add "-host=$NEW_CONFIG_HOST" "-id=$NEW_CONFIG_ID" "-key=$NEW_CONFIG_KEY" 2>&1)
+  display_config
+}
+
 # 倒计时进入脚本
 open_shell() {
-	for((i=3;i>0;i--));do
+	for i in 3 2 1;do
 		echo -ne "\r$i 秒后进入脚本";
 		sleep 1
 	done
@@ -231,10 +376,14 @@ open_shell() {
 # 更新脚本
 upgrade_shell() {
 	download $SHELL_URL /usr/bin/flybit
-	success "更新成功"
 	chmod +x /usr/bin/flybit
-	open_shell
-	exit 0
+  success "更新成功"
+	if [ $# -eq 0 ]; then
+    exit 0;
+  else
+    TEMP_MSG="更新成功"
+    open_shell
+  fi
 }
 
 # 安装脚本
@@ -247,9 +396,10 @@ install_shell() {
 			cp "$SHELL_FOLDER/${0##*/}" /usr/bin/flybit
 			chmod +x /usr/bin/flybit
 			success "成功安装脚本到/usr/bin/flybit！"
-			success "输入flybit即可运行脚本"
-			open_shell
-			exit 0
+      success "输入flybit即可运行脚本"
+			if [ $# -eq 0 ]; then
+			  open_shell
+			fi
 		fi
 	fi
 }
@@ -268,14 +418,127 @@ download() {
 	wget -L -O "$2" "$1?$(date +%s)"
 }
 
-install_shell
+# 命令行帮助
+help() {
+  clear
+  echo $LINE
+  success "Flybit脚本帮助"
+  echo $LINE
+  success "* 交互操作： flybit"
+  echo $LINE
+  success "* 查看信息： flybit info"
+  success "* 查看帮助： flybit help"
+  success "* 查看日志： flybit log"
+  success "* 清空日志： flybit clear"
+  success "* 启动服务： flybit start"
+  success "* 关闭服务： flybit stop"
+  success "* 重启服务： flybit restart"
+  success "* 安装服务： flybit install"
+  success "* 卸载服务： flybit uninstall"
+  success "* 更新服务和脚本： flybit upgrade"
+  success "* 卸载服务和脚本： flybit uninstallAll"
+  echo $LINE
+  success "* 查看配置 flybit config"
+  success "* 删除配置 flybit config remove 1"
+  info "  后面的1替换成查看配置中对应的序号"
+  info "  删除配置之后原来的配置序号会发生改变"
+  success "* 新增配置 flybit config add -host=通信地址 -id=ID -key=密钥"
+  info "  新增配置，如果域名已存在会替换原来的配置"
+#  echo "----------------------------------"
+#  info "说明："
+#  info "1. 新增配置，如果域名已存在会替换原来的配置"
+#  info "2. 删除配置之后原来的配置序号会发生改变"
+  echo $LINE
+}
+
+# 检查
+check
+
 
 # 判断脚本执行时有没有参数
 if [ $# -eq 0 ]; then
+  # 安装脚本
+  install_shell
   # 如果没有参数，则调用 display_menu 方法
   display_menu
 else
-  # 如果有参数，则提示用户如何使用
-  echo "用法: $0  (不带任何参数显示菜单)"
-  exit 1 # 以非零状态退出，表示有错误
+  # 安装脚本
+  install_shell 1
+  # 输出版本和状态信息
+  get_service_status "flybit"
+  status_code=$?
+  # 处理用户输入
+  case "$1" in
+    # 帮助
+    "help"|"-h")help;;
+    # 查看日志
+    "logs"|'log')
+      clear
+      tail -fn 30 $FLYBIT_HOME/logs/info.log
+      ;;
+    "info"|"-i")
+      clear
+      echo $LINE
+      success "当前脚本版本: V$VERSION"
+      if [[ "1" != "${status_code}" ]]; then
+        server_version=$("$FLYBIT_HOME/flybit-agent" version 2>&1)
+        success "当前服务版本: V$server_version"
+        if [[ "2" = "${status_code}" ]]; then
+          err "当前状态：服务未启动"
+        else
+          success "当前状态：服务运行中"
+        fi
+      else
+        err "当前状态：未安装服务"
+      fi
+      echo $LINE
+      ;;
+    "clear")
+      rm $FLYBIT_HOME/logs/*
+      if [[ "3" = "${status_code}" ]]; then
+        systemctl restart flybit
+      fi
+      ;;
+    # 卸载
+    "uninstall")
+      uninstall
+      ;;
+    "uninstallAll")
+      uninstall
+      uninstall_shell
+      ;;
+    # 安装服务
+    "install") install;;
+    # 更新
+    "upgrade" | "update")
+      upgrade_shell
+      systemctl stop flybit
+      install
+      ;;
+    # 启动服务
+    "start") systemctl start flybit;;
+    # 停止服务
+    "stop") systemctl stop flybit;;
+    # 重启服务
+    "restart") systemctl restart flybit;;
+    # 配置
+    "config")
+      echo $LINE
+      cd $FLYBIT_HOME
+      command_output=$(./flybit-agent config "${@:2}")
+      exit_code=$?
+      if [ $exit_code -eq 0 ]; then
+        success "$command_output"
+      else
+        err "$command_output"
+      fi
+      if [[ "3" = "${status_code}" ]]; then
+        systemctl restart flybit
+      fi
+      echo $LINE
+      ;;
+    *)
+      err "错误的指令！"
+      ;;
+  esac
 fi
